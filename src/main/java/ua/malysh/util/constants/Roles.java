2ec36e7c18c34caa770818415da6d82c9696933e
MalyshEvhen <102974676+MalyshEvhen@users.noreply.github.com>
2023-06-23 13:15:59 +0300

    Exclude constants to separate util classes

---
+++ src/main/java/ua/malysh/util/constants/Roles.java
@@ -0,0 +1,6 @@
+package ua.malysh.util.constants;
+
+public class Roles {
+    public static final String USER = "ROLE_USER";
+    public static final String ADMIN = "ROLE_ADMIN";
+}
---
+++ src/main/java/ua/malysh/util/constants/Scopes.java
@@ -0,0 +1,6 @@
+package ua.malysh.util.constants;
+
+public class Scopes {
+    public static final String READ = "SCOPE_read";
+    public static final String WRITE = "SCOPE_write";
+}
